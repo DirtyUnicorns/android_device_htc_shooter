97c268145083e49f539139af2b016e11bde54404
Alex Cruz <mazdarider23@gmail.com>
2014-09-28 23:54:46 -0400

    DU-ify

--- du.mk
+++ du.mk
@@ -2,8 +2,8 @@
 PRODUCT_PACKAGES := \
     Camera
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common DU stuff.
+$(call inherit-product, vendor/du/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/htc/shooter/shooter.mk)
@@ -12,7 +12,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_shooter BUILD_FINGERPRINT="htc/
 
 # Device naming
 PRODUCT_DEVICE := shooter
-PRODUCT_NAME := cm_shooter
+PRODUCT_NAME := du_shooter
 PRODUCT_BRAND := sprint
 PRODUCT_MANUFACTURER := HTC
 PRODUCT_MODEL := Evo 3D CDMA
@@ -21,6 +21,3 @@ PRODUCT_MODEL := Evo 3D CDMA
 TARGET_SCREEN_HEIGHT := 960
 TARGET_SCREEN_WIDTH := 540
 
-# Release name
-PRODUCT_RELEASE_NAME := shooter
--include vendor/cm/config/common_versions.mk
